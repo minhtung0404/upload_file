5b90dc384f896330daea0a21c87e2146157e0a09
minhtung0404 <minhtung04042001@gmail.com>
2020-08-22 16:29:55 +0700

    adjust

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ var http = require('http');
 var fs = require('fs');
 var express = require('express');
 var formidable = require('formidable');
+var path = require('path');
 var app = express();
 
-app.use('/', express.static('/'));
+app.use(express.static('public'));
 
 let Data, Upload, Download;
 
-fs.readFile('./html/index.html', 'utf8', (err, data) => {
+fs.readFile('./public/html/index.html', 'utf8', (err, data) => {
     if (err) throw err;
     Data = data;
 });
 
-fs.readFile('./html/download.html', 'utf8', (err, data) => {
+fs.readFile('./public/html/download.html', 'utf8', (err, data) => {
     if (err) throw err;
     Download = data;
 });
 
-fs.readFile('./html/upload.html', 'utf8', (err, data) => {
+fs.readFile('./public/html/upload.html', 'utf8', (err, data) => {
     if (err) throw err;
     Upload = data;
 });
@@ -29,7 +30,7 @@ app.get('/', function(req, res){
 });
 
 app.get('/download', function(req, res){
-    fs.readdir('./files/', (err, files) =>{
+    fs.readdir('./public/files/', (err, files) =>{
         if (err) throw err;
         if (files.length > 0){
             let html = '<a class = "link">Here is all the files: </a><br>\n';
@@ -50,7 +51,7 @@ app.get('/download', function(req, res){
 });
 
 app.get('/files/:filename', function(req, res){
-    res.download('./files/' + req.params.filename, (err) => {
+    res.download('./public/files/' + req.params.filename, (err) => {
         if (err) throw err;
         console.log("Download " + req.params.filename + " completed!");
         res.end();
@@ -66,7 +67,7 @@ app.post('/upload', function(req, res){
     var form = new formidable.IncomingForm();
     form.parse(req, function(err, fields, files){
         var oldpath = files.upload.path;
-        var newpath = './files/' + files.upload.name;
+        var newpath = './public/files/' + files.upload.name;
         fs.rename(oldpath, newpath, function (err) {
             if (err) throw err;
             res.redirect('/');
